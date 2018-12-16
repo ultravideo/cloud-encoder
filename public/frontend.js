e15ad88b70dc90686686dd37dbe9096d29f99788
Aaro Altonen <aaro.altonen@tut.fi>
2018-12-16 14:32:31 +0200

    Start using bootstrap for UI

--- public/frontend.js
+++ public/frontend.js
@@ -98,173 +98,202 @@ if(!r.support) {
     $('.resumable-error').show();
 } else {
     r.assignBrowse($('#browseButton'));
-
-    $('#submitButton').click(function(){
-        if (numFiles == 0) {
-            console.log("no files");
-            alert("select file!");
-            return;
-        }
-
-        var other_options = {}, kvz_options = {};
-        $(".kvz_options").serializeArray().map(function(x){kvz_options[x.name] = x.value;});
-        $(".options").serializeArray().map(function(x){other_options[x.name] = x.value;});
-
-        var options = { 'type' : 'options', 'token': userToken, 'kvazaar' : kvz_options, 'other' : other_options };
-        options['other']['file_id'] = fileID;
-
-        if (other_options.raw_video && other_options.raw_video === "on" && other_options.resolution === "") {
-            document.getElementById("resMissing").style.display = "block";
-            return;
-        } else {
-            document.getElementById("resMissing").style.display = "none";
-        }
-
-        console.log("sent options...");
-        connection.send(JSON.stringify(options));
-    });
-
-    // Handle file add event
-    r.on('fileAdded', function(file){
-        // remove previously selected files
-        $('.resumable-list').empty();
-        r.files = r.files.slice(-1);
-        console.log(r.files);
-
-        $('.resumable-progress, .resumable-list').show();
-        $('.resumable-progress .progress-resume-link').hide();
-        $('.resumable-progress .progress-pause-link').hide();
-        $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html(0 + '%');
-        $('.progress-bar').css({width:0 + '%'});
-
-        // Add the file to the list
-        $('.resumable-list').append('<li class="resumable-file-'+file.uniqueIdentifier+'">'
-            + '<span class="resumable-file-name"></span> <span class="resumable-file-progress">ready for upload</span>');
-        $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-name').html(file.fileName);
-
-        fileID = file.uniqueIdentifier;
-        numFiles = 1;
-
-        let fname = r.files[r.files.length - 1].fileName.toString();
-
-        // set raw video to true if file extension is yuv
-        if ($("#rawVideoCheck").is(":checked") === false) {
-            let res  = fname.match(/\.yuv$/g);
-
-            if (res && res.length != 0) {
-                $("#rawVideoCheck").click();
-            } 
-        } else {
-            let res  = fname.match(/\.yuv$/g);
-
-            if (!res) {
-                $("#rawVideoCheck").click();
-                $("#resValue").val("");
-            } 
-        }
-
-        // try to match file resolution from name
-        let res  = fname.match(/[0-9]{1,4}\x[0-9]{1,4}/g);
-        if (res && res.length != 0) {
-            $("#resValue").val(res[0]);
-        } 
-
-        $("#submitButton").prop("disabled", false);
-    });
-
-    r.on('pause', function(){
-        // Show resume, hide pause
-        $('.resumable-progress .progress-resume-link').show();
-        $('.resumable-progress .progress-pause-link').hide();
-    });
-
-    r.on('complete', function(){
-        // Hide pause/resume when the upload has completed
-        $('.resumable-progress .progress-resume-link, .resumable-progress .progress-pause-link').hide();
-    });
-
-    r.on('fileSuccess', function(file,message){
-        // Reflect that the file upload has completed
-        $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html('(completed)');
-    });
-
-    r.on('fileError', function(file, message){
-        // Reflect that the file upload has resulted in error
-        $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html('(file could not be uploaded: '+message+')');
-    });
-
-    r.on('fileProgress', function(file){
-        // Handle progress for both the file and the overall upload
-        $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html(Math.floor(file.progress()*100) + '%');
-        $('.progress-bar').css({width:Math.floor(r.progress()*100) + '%'});
-    });
-
-    r.on('cancel', function(){
-        $('.resumable-file-progress').html('canceled');
-        $("#submitButton").prop("disabled", true);
-        numFiles = 0;
-    });
-
-    r.on('uploadStart', function(){
-        // Show pause, hide resume
-        $('.resumable-progress .progress-resume-link').hide();
-        $('.resumable-progress .progress-pause-link').show();
-    });
-
-    // WebSocket stuff
-    connection.onopen = function() {
-        console.log("connection established");
-
-        // generate token for this connection so server knows to
-        // send status updates to correct client
-        let message = {
-            type: "init",
-            token: userToken
-        };
-
-        connection.send(JSON.stringify(message));
-        console.log(JSON.stringify(message));
-    };
-
-    connection.onmessage = function(message) {
-        var message_data = null;
-
-        try {
-            message_data = JSON.parse(message.data);
-        } catch (e) {
-            console.log(message.data);
-            console.log(e);
-            return;
-        }
-
-        if (message_data.message != null) {
-            appendToDiv(message_data);
-        }
-
-        // server send us message regarding resumable upload process
-        if (message_data.type === "action") {
-            if (message_data.reply == "upload") {
-                $('.resumable-progress .progress-resume-link').hide();
-                $('.resumable-progress .progress-pause-link').show();
-                r.upload();
-            } else if (message_data.reply == "cancel") {
-                r.cancel();
-                $('.resumable-list').empty();
-            } else if (message_data.reply == "pause") {
-                r.pause();
-            } else if (message_data.reply == "continue") {
-                r.upload();
-            } else if (message_data.reply === "download") {
-                downloadFile(message_data);
-            }
-        }
-    };
-
-    connection.onclose = function(error) {
-        console.log("connection closed...");
-    };
-
-    connection.onerror = function(error) {
-        console.log(error);
-    };
 }
+
+$('#submitButton').click(function(){
+    if (numFiles == 0) {
+        console.log("no files");
+        alert("select file!");
+        return;
+    }
+
+    var other_options = {}, kvz_options = {};
+    $(".kvz_options").serializeArray().map(function(x){kvz_options[x.name] = x.value;});
+    $(".options").serializeArray().map(function(x){other_options[x.name] = x.value;});
+
+    var options = { 'type' : 'options', 'token': userToken, 'kvazaar' : kvz_options, 'other' : other_options };
+    options['other']['file_id'] = fileID;
+
+    if (other_options.raw_video && other_options.raw_video === "on" && other_options.resolution === "") {
+        document.getElementById("resMissing").style.display = "block";
+        return;
+    } else {
+        document.getElementById("resMissing").style.display = "none";
+    }
+
+    console.log("sent options...");
+    connection.send(JSON.stringify(options));
+});
+
+function deActivate(name) {
+    $("#div" + name).hide();
+    $("#li" + name).removeClass("active");
+}
+
+function activateView(name) {
+    $("#div" + name).show();
+    $("#li" + name).addClass("active");
+}
+
+$("#linkUpload").click(function() {
+    deActivate("About");
+    deActivate("Requests");
+    activateView("Upload")
+});
+
+$("#linkRequests").click(function() {
+    deActivate("About");
+    deActivate("Upload");
+    activateView("Requests")
+});
+
+$("#linkAbout").click(function() {
+    deActivate("Requests");
+    deActivate("Upload");
+    activateView("About")
+});
+
+// ------------------------------- Resumablejs stuff ------------------------------- 
+r.on('fileAdded', function(file){
+    // remove previously selected files
+    $('.resumable-list').empty();
+    r.files = r.files.slice(-1);
+    console.log(r.files);
+
+    $('.resumable-progress, .resumable-list').show();
+    $('.resumable-progress .progress-resume-link').hide();
+    $('.resumable-progress .progress-pause-link').hide();
+    $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html(0 + '%');
+    $('.progress-bar').css({width:0 + '%'});
+
+    // Add the file to the list
+    $('.resumable-list').append('<li class="resumable-file-'+file.uniqueIdentifier+'">'
+        + '<span class="resumable-file-name"></span> <span class="resumable-file-progress">ready for upload</span>');
+    $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-name').html(file.fileName);
+
+    fileID = file.uniqueIdentifier;
+    numFiles = 1;
+
+    let fname = r.files[r.files.length - 1].fileName.toString();
+
+    // set raw video to true if file extension is yuv
+    if ($("#rawVideoCheck").is(":checked") === false) {
+        let res  = fname.match(/\.yuv$/g);
+
+        if (res && res.length != 0) {
+            $("#rawVideoCheck").click();
+        } 
+    } else {
+        let res  = fname.match(/\.yuv$/g);
+
+        if (!res) {
+            $("#rawVideoCheck").click();
+            $("#resValue").val("");
+        } 
+    }
+
+    // try to match file resolution from name
+    let res  = fname.match(/[0-9]{1,4}\x[0-9]{1,4}/g);
+    if (res && res.length != 0) {
+        $("#resValue").val(res[0]);
+    } 
+
+    $("#submitButton").prop("disabled", false);
+});
+
+r.on('pause', function(){
+    // Show resume, hide pause
+    $('.resumable-progress .progress-resume-link').show();
+    $('.resumable-progress .progress-pause-link').hide();
+});
+
+r.on('complete', function(){
+    // Hide pause/resume when the upload has completed
+    $('.resumable-progress .progress-resume-link, .resumable-progress .progress-pause-link').hide();
+});
+
+r.on('fileSuccess', function(file,message){
+    // Reflect that the file upload has completed
+    $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html('(completed)');
+});
+
+r.on('fileError', function(file, message){
+    // Reflect that the file upload has resulted in error
+    $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html('(file could not be uploaded: '+message+')');
+});
+
+r.on('fileProgress', function(file){
+    // Handle progress for both the file and the overall upload
+    $('.resumable-file-'+file.uniqueIdentifier+' .resumable-file-progress').html(Math.floor(file.progress()*100) + '%');
+    $('.progress-bar').css({width:Math.floor(r.progress()*100) + '%'});
+});
+
+r.on('cancel', function(){
+    $('.resumable-file-progress').html('canceled');
+    $("#submitButton").prop("disabled", true);
+    numFiles = 0;
+});
+
+r.on('uploadStart', function(){
+    // Show pause, hide resume
+    $('.resumable-progress .progress-resume-link').hide();
+    $('.resumable-progress .progress-pause-link').show();
+});
+
+
+// ------------------------------- WebSocket stuff ------------------------------- 
+connection.onopen = function() {
+    console.log("connection established");
+
+    // generate token for this connection so server knows to
+    // send status updates to correct client
+    let message = {
+        type: "init",
+        token: userToken
+    };
+
+    connection.send(JSON.stringify(message));
+    console.log(JSON.stringify(message));
+};
+
+connection.onmessage = function(message) {
+    var message_data = null;
+
+    try {
+        message_data = JSON.parse(message.data);
+    } catch (e) {
+        console.log(message.data);
+        console.log(e);
+        return;
+    }
+
+    if (message_data.message != null) {
+        appendToDiv(message_data);
+    }
+
+    // server send us message regarding resumable upload process
+    if (message_data.type === "action") {
+        if (message_data.reply == "upload") {
+            $('.resumable-progress .progress-resume-link').hide();
+            $('.resumable-progress .progress-pause-link').show();
+            r.upload();
+        } else if (message_data.reply == "cancel") {
+            r.cancel();
+            $('.resumable-list').empty();
+        } else if (message_data.reply == "pause") {
+            r.pause();
+        } else if (message_data.reply == "continue") {
+            r.upload();
+        } else if (message_data.reply === "download") {
+            downloadFile(message_data);
+        }
+    }
+};
+
+connection.onclose = function(error) {
+    console.log("connection closed...");
+};
+
+connection.onerror = function(error) {
+    console.log(error);
+};
