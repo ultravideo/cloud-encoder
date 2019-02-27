daa412bc6e54375e18c2bca083bb1456bfafb2aa
Marko Viitanen <fador@iki.fi>
2019-02-27 15:11:56 +0200

    Allow more precise fps num/denom

--- app/parser.js
+++ app/parser.js
@@ -57,7 +57,7 @@ module.exports = {
 
     validateFrameRate : function(str) {
         return new Promise((resolve, reject) => {
-            let fps = str.match(/[0-9]{1,4}\/[0-9]{1,4}/g);
+            let fps = str.match(/[1-9]{1,9}\/[0-9]{1,9}/);
             if (fps && fps.length > 0)
                 resolve(fps[0]);
             reject(new Error("Invalid FPS!"));
